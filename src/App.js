dfe4b255116ceed89977288d9ccf7132c7f11dc4
Forrest Walker <forrest.walker@forrestwalker.com>
2022-07-02 06:59:44 -0500

    Removed bg color from tab list

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
             <Header/>
             <main className={"flex flex-col justify-center w-full"}>
                 <section className="max-w-screen-xl mt-10 bg-slate-800 rounded-tl-md rounded-tr-md ml-4 mr-4 xl:ml-auto xl:mr-auto xl:w-full relative">
-                    <ul className="flex flex-col justify-center text-center text-white transition-all lg:flex-row bg-slate-800 ">
+                    <ul className="flex flex-col justify-center text-center text-white transition-all lg:flex-row">
                         <li className={`${isHome ? ' border-t-green-500  bg-slate-700 ' : 'border-t-slate-800'} rounded-tr-md rounded-tl-md lg:rounded-none duration-300 ease-in-out transition-all border-t-4 text-2xl w-full lg:w-40 p-3`}><Link to={"/"}>Home</Link></li>
                         <li className={`${isRules ? ' border-t-green-500 bg-slate-700' : 'border-t-slate-800'} duration-300 ease-in-out transition-all border-t-4 text-2xl w-full lg:w-40 p-3`}><Link to={"/rules"}>Rules</Link></li>
                         <li className={`${isMods ? ' border-t-green-500 bg-slate-700' : 'border-t-slate-800'} duration-300 ease-in-out transition-all border-t-4 text-2xl w-full lg:w-40 p-3`}><Link to={"/mods"}>Mods</Link></li>
